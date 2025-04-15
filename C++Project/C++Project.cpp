c48014551cca37b0fc5e257a0c7355711cd31962
Josephlodico <164060090+Josephlodico@users.noreply.github.com>
2025-04-14 20:11:08 -0400

    made a class RPSgame in Game.h header file meaning (Rock,Paper,Scizzors) for the game

--- C++Project/C++Project.cpp
+++ C++Project/C++Project.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "shop.h" 
 #include "Money.h" 
+#include "Game.h"
 
 
 
--- C++Project/Game.h
+++ C++Project/Game.h
@@ -1,7 +1,17 @@
-#pragma once
+#pragma once    
 #include <iostream>
+#include <string>
 #include "Money.h"
-
-class Game {
-
+// Rock,Paper,Sizzors
+class RPSGame {
+public:
+	std::string playRound(std::string playerchoice);
+	int getyourEarnings() const;
+private:
+	int earnings;
+	std::string getComputerChoice();
+	int determineTheWinner(std::string playerchoice, std::string computerChoice);
+	
 };
+
+
