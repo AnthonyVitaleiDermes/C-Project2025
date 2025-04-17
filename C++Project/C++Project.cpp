a6b908d7d5bc3d6185e6543eaed7670a5356b627
Joseph Lodico <josephl2020@hotmail.com>
2025-04-17 18:45:35 -0400

    fixed it so that you could go back and pick case 1 or 2 again

--- C++Project/C++Project.cpp
+++ C++Project/C++Project.cpp
@@ -18,50 +18,52 @@ int main() // yur 5"3 asain girl abg >>
     Money userMoney;
     userMoney.setAmount(100.00f);
  
-    int action;
-
-    std::cout << "Welcome To the online store and games\n";
-    std::cout << "Press 1 for the Shop and 2 for the Minigame to earn money!\n";
-    
-    std::cin >> action;
-
-
     Shop<int, float> shopCity;
     RPSGame game;
+    while (true) {
 
-    switch (action)
-    {
-    case 1:
-        shopCity.displayShopMenu(userMoney);
-        shopCity.interact(userMoney);
+        int action;
 
-       
-      
-        break;
-    case 2: {
-        RPSGame game;
-        std::string choice;
-        std::cout << "Welcome To The Minigame! Earn or lose money by playing Rock-Paper-Scizzors.\n";
-
-        while (true) {
-            std::cout << "Enter rock,paper, or scizzors (or 'quit' to exit):";
-            std::cin >> choice;
-
-            if (choice == "quit") break;
-
-            std::cout << game.playRound(choice) << std::endl;
-            game.EarningsDisplayed();
+        std::cout << "Welcome To the online store and games\n";
+        std::cout << "Press 1 for the Shop and 2 for the Minigame to earn money!\n";
+    
+        std::cin >> action;
+        if (action == 0) {
+            std::cout << "Thanks for playing! Bye Bye\n";
+            break;
         }
-        std::cout << "Final money Amount: ";
-        userMoney.setAmount(game.getEarnings()); // update's the user's money balance
-        userMoney.displayMoney();
 
-        //Minigame Method
-        break;
-    }
-    default:
-        std::cout << "Invalid Option\n";
-        break;
+        switch (action)
+        {
+        case 1:
+            shopCity.displayShopMenu(userMoney);
+            shopCity.interact(userMoney);
+            break;
+        case 2: {
+            RPSGame game;
+            std::string choice;
+            std::cout << "Welcome To The Minigame! Earn or lose money by playing Rock-Paper-Scizzors.\n";
+
+            while (true) {
+                std::cout << "Enter rock,paper, or scizzors (or 'quit' to exit):";
+                std::cin >> choice;
+
+                if (choice == "quit") break;
+
+                std::cout << game.playRound(choice) << std::endl;
+                game.EarningsDisplayed();
+            }
+            std::cout << "Final money Amount: ";
+            userMoney.setAmount(game.getEarnings()); // update's the user's money balance
+            userMoney.displayMoney();
+            //Minigame Method
+            break;
+        }
+        default:
+            std::cout << "Invalid Option\n";
+            break;
+        }
+
     }
 
     return 0;
